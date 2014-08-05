08abaf99c04dfa6d23754139358cb38619eb067a
Jordan Haines <jordan@jordanairwave.co.uk>
2014-08-05 09:50:02 +0100

    scrolling function added to js file for main navigation
    Signed-off-by: Jordan Haines <jordan@jordanairwave.co.uk>

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,20 @@ var site = {
     		responsive: true,
     		positionProperty: 'transform'
     	});
+    	
+		//Main Navigation scrolling Function
+		$('#main-nav ul li a').on('click', function(event) {
+			event.preventDefault();
+			site.section = $(this).attr('data-section');
+			
+			//add current clase to clicked item and remove from all others
+			$(this).parent().addClass('current').siblings().removeClass('current');
+			
+			//Scroll to the section requested
+			$('html, body').animate({
+				scrollTop: $('#' + site.section).offset().top - 100
+			},'slow');
+		});
 
     }
 };
